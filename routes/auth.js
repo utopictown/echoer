0895b854b6acfd6b3cb1a90a61a9d5233ad854df
utopictown <ekoprstyo.mail@gmail.com>
2020-11-08 16:08:18 +0700

    use app_url

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/auth/facebook/callback",
+      callbackURL: process.env.APP_URL + "/auth/facebook/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
